1256b69b3cb8a60dc5e3715985cc7c2a26d5b0cc
unknown <michaelabebe19922000@gmail.com>
2022-09-07 17:18:38 +0300

    working react carousel

--- src/inc/Slider.js
+++ src/inc/Slider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 import Zefmesh from "../Images/zefmesh.png";
 import Taye from "../Images/Taye.png";
 import Getfam from "../Images/getfam-hotel.png";
@@ -10,47 +11,23 @@ function Slider() {
     <div>
       <div class="companies">
         <h2>Companies we have worked with</h2>
-        <div
-          id="carouselExampleDark"
-          class="carousel carousel-dark slide"
-          data-bs-ride="carousel"
-        >
-          <div class="carousel-inner">
-            <div class="carousel-item" data-bs-interval="3000">
-              <img src={Getfam} class="d-block" alt="..." />
-            </div>
-            <div class="carousel-item active" data-bs-interval="3000">
-              <img src={Abay} class="d-block" alt="..." />
-            </div>
-            <div class="carousel-item" data-bs-interval="3000">
-              <img src={Zefmesh} class="d-block" alt="..." />
-            </div>
-            <div class="carousel-item" data-bs-interval="3000">
-              <img src={Taye} class="d-block" alt="..." />
-            </div>
-            <div class="carousel-item" data-bs-interval="3000">
-              <img src={Ambassador} class="d-block" alt="..." />
-            </div>
-          </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
+        <Carousel indicators={false} variant="dark">
+          <Carousel.Item>
+            <img src={Getfam} class="d-block" alt="..." />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={Abay} class="d-block" alt="..." />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={Zefmesh} class="d-block" alt="..." />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={Taye} class="d-block" alt="..." />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={Ambassador} class="d-block" alt="..." />
+          </Carousel.Item>
+        </Carousel>
       </div>
     </div>
   );
